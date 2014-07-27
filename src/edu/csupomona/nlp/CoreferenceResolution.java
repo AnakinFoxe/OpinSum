0f8d4574e4d647bd45591f66f0ce3f6cf8da2b2a
AnakinFoxe <xing.y.hu@gmail.com>
2014-07-26 20:47:48 -0700

    Tested Coreference Resolution

--- src/edu/csupomona/nlp/CoreferenceResolution.java
+++ src/edu/csupomona/nlp/CoreferenceResolution.java
@@ -8,20 +8,11 @@ package edu.csupomona.nlp;
 
 import edu.stanford.nlp.dcoref.CorefChain;
 import edu.stanford.nlp.dcoref.CorefCoreAnnotations.CorefChainAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
-import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
-import edu.stanford.nlp.util.CoreMap;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 
@@ -31,51 +22,37 @@ import java.util.Properties;
  */
 public class CoreferenceResolution {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
-    Properties props = new Properties();
-    props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-    // read some text in the text variable
-    String text = "I thought about waiting on the Lumia 925, but after reading how much the Lumina 521 has to offer, I decided that since I tend to change phones about every 2 years, it made more sense to go with the phone that was lighter on my wallet."; // Add your text here!
+        // read some text in the text variable
+        String text = "";
+        FileReader fr = new FileReader("./data/reviews/pre-processed/nokia-lumia521-tmobile/3.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.trim().length() > 6) {
+                text += line + " ";
+            }
+        }
 
-    // create an empty Annotation just with the given text
-    Annotation document = new Annotation(text);
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
 
-    // run all Annotators on this text
-    pipeline.annotate(document);
+        // run all Annotators on this text
+        pipeline.annotate(document);
 
-    // these are all the sentences in this document
-    // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-
-    for(CoreMap sentence: sentences) {
-      // traversing the words in the current sentence
-      // a CoreLabel is a CoreMap with additional token-specific methods
-      for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
-        // this is the text of the token
-        String word = token.get(TextAnnotation.class);
-        // this is the POS tag of the token
-        String pos = token.get(PartOfSpeechAnnotation.class);
-        // this is the NER label of the token
-        String ne = token.get(NamedEntityTagAnnotation.class);       
-      }
-
-      // this is the parse tree of the current sentence
-      Tree tree = sentence.get(TreeAnnotation.class);
-
-      // this is the Stanford dependency graph of the current sentence
-      SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
-      
-    }
-
-    // This is the coreference link graph
-    // Each chain stores a set of mentions that link to each other,
-    // along with a method for getting the most representative mention
-    // Both sentence and token offsets start at 1!
-    Map<Integer, CorefChain> graph = document.get(CorefChainAnnotation.class);
-    System.out.println(graph);
+        // This is the coreference link graph
+        // Each chain stores a set of mentions that link to each other,
+        // along with a method for getting the most representative mention
+        // Both sentence and token offsets start at 1!
+        Map<Integer, CorefChain> graph = document.get(CorefChainAnnotation.class);
+        for (Integer n : graph.keySet()) {
+            System.out.println(graph.get(n).getMentionMap());
+        }
     
 
         
