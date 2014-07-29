36b56451f0f071ba4cb1331c942395c7718bd9a9
AnakinFoxe <xing.y.hu@gmail.com>
2014-07-28 22:40:50 -0700

    Able to get relation name, governor and dependant now, including the index

--- src/edu/csupomona/nlp/Sentence2Clause.java
+++ src/edu/csupomona/nlp/Sentence2Clause.java
@@ -7,13 +7,21 @@
 package edu.csupomona.nlp;
 
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.IndexedWord;
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedDependenciesAnnotation;
-import edu.stanford.nlp.semgraph.SemanticGraphEdge;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.BasicDependenciesAnnotation;
+import edu.stanford.nlp.trees.GrammaticalStructure;
+import edu.stanford.nlp.trees.GrammaticalStructureFactory;
+import edu.stanford.nlp.trees.PennTreebankLanguagePack;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreebankLanguagePack;
+import edu.stanford.nlp.trees.TypedDependency;
 import edu.stanford.nlp.util.CoreMap;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
@@ -46,13 +54,34 @@ public class Sentence2Clause {
         for(CoreMap sentence: sentences) {
 
             // this is the Stanford dependency graph of the current sentence
-            SemanticGraph dependencies = sentence.get(CollapsedDependenciesAnnotation.class);
+            SemanticGraph dependencies = sentence.get(BasicDependenciesAnnotation.class);
 
             System.out.println(dependencies.toString("plain"));
+//            dependencies.
 //            for (SemanticGraphEdge edge : dependencies.getEdgeSet()) {
 //                System.out.println(edge.toString());
 //            }
         }
-
+        
+        
+        
+        LexicalizedParser lp = LexicalizedParser.loadModel(
+            "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz",
+            "-maxLength", "80", "-retainTmpSubcategories");
+        TreebankLanguagePack tlp = new PennTreebankLanguagePack();
+        GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+        String[] sent = "I have this phone linked to my business Exchange account, and use this phone for work regularly .".split(" ");
+        Tree parse = lp.apply(Sentence.toWordList(sent));
+        GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
+        Collection<TypedDependency> tdl = gs.typedDependencies();
+        System.out.println(tdl);
+        Iterator<TypedDependency> itr = tdl.iterator();
+        while (itr.hasNext()) {
+            TypedDependency td = itr.next();
+            System.out.println(td.reln().getShortName() + " GOV:" + td.gov().value() + "==" + td.gov().index() + "  DEP:" + td.dep().toString());
+            
+        }
     }
+    
+
 }
